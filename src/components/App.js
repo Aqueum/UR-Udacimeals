83373121290b76f2aa352d55520e7913d52bcee0
Aqueum <martin.currie@aqueum.com>
2018-03-23 11:02:49 +0000

    func: Connect app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log('Props', this.props);
     return <div>Hello World</div>;
   }
 }
 
-export default App;
+export default connect()(App);
